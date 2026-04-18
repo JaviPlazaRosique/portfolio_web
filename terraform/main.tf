2db9b6348171ebf9f334e767193242e1e67b5883
JaviPlazaRosique <j.plazarosique@gmail.com>
2026-04-19 00:28:37 +0200

    .

--- terraform/main.tf
+++
@@ -1,7 +0,0 @@
-module "github_pages" {
-  source = "./modules/github_pages"
-
-  repository_name = var.repository_name
-  branch          = var.pages_branch
-  path            = var.pages_path
-}
--- terraform/modules/github_pages/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "pages_url" {
-  description = "URL of the published GitHub Pages site"
-  value       = try(github_repository.this.pages[0].html_url, null)
-}
